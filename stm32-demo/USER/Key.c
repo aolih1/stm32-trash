36453e3193331757af719f60875b9f593971c5cd
aolih1 <3372136125@qq.com>
2025-04-10 10:04:36 +0800

    v3 未添加远程连接

--- stm32-demo/USER/Key.c
+++ stm32-demo/USER/Key.c
@@ -1,10 +1,74 @@
 // 按键逻辑控制
 
 #include "stm32f10x.h"
-#include "Key.h"
+
 
 
 void Key_Init()
 {
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB, ENABLE);	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO,ENABLE);
 	
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 ;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);							//引脚初始化为上拉输入
+	
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7 ;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);	
+	
+	GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource0 | GPIO_PinSource1);
+	GPIO_EXTILineConfig(GPIO_PortSourceGPIOA, GPIO_PinSource7);
+	
+	EXTI_InitTypeDef EXTI_InitStructure;						//定义结构体变量
+	EXTI_InitStructure.EXTI_Line = EXTI_Line0 | EXTI_Line1 | EXTI_Line7 ;					//选择配置外部中断的14号线
+	EXTI_InitStructure.EXTI_LineCmd = ENABLE;					//指定外部中断线使能
+	EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;			//指定外部中断线为中断模式
+	EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;		//指定外部中断线为下降沿触发
+	EXTI_Init(&EXTI_InitStructure);			
+	
+	
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);				//配置NVIC为分组2
+																//即抢占优先级范围：0~3，响应优先级范围：0~3
+																//此分组配置在整个工程中仅需调用一次
+																//若有多个中断，可以把此代码放在main函数内，while循环之前
+																//若调用多次配置分组的代码，则后执行的配置会覆盖先执行的配置
+	
+	/*NVIC配置*/
+	NVIC_InitTypeDef NVIC_InitStructure;						//定义结构体变量
+	NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn |EXTI1_IRQn | EXTI9_5_IRQn;		//选择配置NVIC的EXTI15_10线
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;				//指定NVIC线路使能
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;	//指定NVIC线路的抢占优先级为1
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;			//指定NVIC线路的响应优先级为1
+	NVIC_Init(&NVIC_InitStructure);								//将结构体变量交给NVIC_Init，配置NVIC外设
+}
+
+void EXTI0_IRQHandler()
+{
+	if (EXTI_GetITStatus(EXTI_Line14) == SET)		//判断是否是外部中断14号线触发的中断
+	{
+		/*如果出现数据乱跳的现象，可再次判断引脚电平，以避免抖动*/
+		if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_0) == 0)
+		{
+			TIM_SetCompare1(TIM2, 1500);
+		}
+		EXTI_ClearITPendingBit(EXTI_Line0);		//清除外部中断14号线的中断标志位
+													//中断标志位必须清除
+													//否则中断将连续不断地触发，导致主程序卡死
+	}
+};
+void EXTI1_IRQHandler()
+{
+		if (EXTI_GetITStatus(EXTI_Line14) == SET)		//判断是否是外部中断14号线触发的中断
+	{
+		/*如果出现数据乱跳的现象，可再次判断引脚电平，以避免抖动*/
+		if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_1) == 0)
+		{
+			TIM_SetCompare2(TIM2, 1500);
+		}
+		EXTI_ClearITPendingBit(EXTI_Line1);		
+													//中断标志位必须清除
+													//否则中断将连续不断地触发，导致主程序卡死
+	}
 }
--- stm32-demo/USER/wifi.h
+++
@@ -1,6 +0,0 @@
-#ifndef __WIFI_H
-#define __WIFI_H
-
-void wifi_init(void);
-
-#endif
